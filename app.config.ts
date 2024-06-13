82f4c490108eeca07da03e2090e42823321bcacc
enricomilli <enricomilli666@gmail.com>
2024-06-13 22:38:50 +0200

    fix: app config

--- app.config.ts
+++ app.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from "@solidjs/start/config";
 import solidSvg from "vite-plugin-solid-svg";
+// import netlify from 'solid-start-netlify';
+// import solidStartNetlify from "solid-start-netlify";
 
 export default defineConfig({
+    ssr: false,
+    server: {
+        baseURL: "/",
+        preset: "static"
+    },
     vite: {
         plugins: [solidSvg()],
     },
